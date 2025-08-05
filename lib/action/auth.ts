8f4585b9ad3ccfe4018fcf27920d3d71a70b9f0a
LuisFerdinand <ferdinandluis88@gmail.com>
2025-08-05 21:22:29 +0700

    implement qstash workflo

--- lib/action/auth.ts
+++ lib/action/auth.ts
@@ -8,6 +8,8 @@ import { signIn } from "@/auth";
 import { headers } from "next/headers";
 import ratelimit from "../ratelimit";
 import { redirect } from "next/navigation";
+import { workflowClient } from "../workflow";
+import config from "../config";
 
 export const signInWithCredential = async (
   params: Pick<AuthCredentials, "email" | "password">,
@@ -65,6 +67,14 @@ export const signUp = async (params: AuthCredentials) => {
       password: hashedPassword,
       universityCard,
       universityId,
+    });
+
+    await workflowClient.trigger({
+      url: `${config.env.prodApiEndpoint}/api/workflow/onboarding`,
+      body: {
+        email,
+        fullName,
+      },
     });
 
     await signInWithCredential({ email, password });
